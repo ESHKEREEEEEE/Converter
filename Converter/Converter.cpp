30cef56490db12a26fe45bc2745256cae1db9c37
ESHKEREEEEEE <146151389+ESHKEREEEEEE@users.noreply.github.com>
2026-02-25 12:48:32 +0700

    old russian length conversion passing tests

--- Converter/Converter.cpp
+++ Converter/Converter.cpp
@@ -20,9 +20,19 @@ public:
     {
         double multiplier;
         switch (type) {
+        //METRIC CONVERSIONS
         case meters:        multiplier = 1;     break;
         case kilometers:    multiplier = 1000;  break;
+
+        //OLD RUSSIAN CONVERSIONS
         case vershok:       multiplier = 0.045; break;
+        case pyad:          multiplier = 0.18;  break;
+        case lokot:         multiplier = 0.48;  break;
+        case arshin:        multiplier = 0.72;  break;
+        case sajen:         multiplier = 2.16;  break;
+        case versta:        multiplier = 2160;  break;
+
+        //UNSUPPORTED
         default: throw std::invalid_argument("Unsupported type");
         }
 
