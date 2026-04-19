607c3d8f74eeb688ab6512c8783a65380c1f0bb9
ESHKEREEEEEE <146151389+ESHKEREEEEEE@users.noreply.github.com>
2026-04-19 22:46:48 +0700

    main menu

--- Converter/Converter.cpp
+++ Converter/Converter.cpp
@@ -2,20 +2,20 @@
 
 enum LengthType {
     kilometers = 1,
-    arshin,
-    inch
+    inch,
+    arshin
 };
 
 enum MassType {
     ton = 1,
-    pud,
-    ounce
+    ounce,
+    pud
 };
 
 enum AreaType {
     square_kilometer = 1,
-    square_arshin,
-    square_mile
+    square_mile,
+    square_arshin
 };
 
 class LengthConverter {
@@ -75,5 +75,119 @@ public:
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Welcome to unit conversion program!" << std::endl;
+
+    std::cout << "What unit do you have? \n 1. Length \n 2. Mass \n 3. Area" << std::endl;
+    int unit;
+    std::cin >> unit;
+
+    int in_type;
+    double amount;
+    int out_type;
+    switch (unit) {
+    case 1: //Length
+        LengthConverter lc;
+
+        std::cout <<
+            "What length type do you have?\n"
+            "===Metric===\n"
+            "1. Kilometers\n\n"
+
+            "===USA===\n"
+            "2. Inch\n\n"
+
+            "===Old russian==\n"
+            "3. Arshin\n"
+            << std::endl;
+        std::cin >> in_type;
+        std::cout << "How much?" << std::endl;
+        std::cin >> amount;
+        std::cout <<
+            "What length type do you need?\n"
+            "===Metric===\n"
+            "1. Kilometers\n\n"
+
+            "===USA===\n"
+            "2. Inch\n\n"
+
+            "===Old russian==\n"
+            "3. Arshin\n"
+            << std::endl;
+        std::cin >> out_type;
+        try {
+            std::cout << "Result " << lc.fromMeters((LengthType)out_type, lc.toMeters((LengthType)in_type, amount)) << std::endl;
+        }
+        catch (std::invalid_argument e) { std::cout << e.what() << std::endl; }
+        break;
+    case 2: //Mass
+        MassConverter mc;
+        std::cout <<
+            "What mass type do you have?\n"
+            "===Metric system===\n"
+            "1. Ton\n\n"
+
+            "===USA===\n"
+            "2. Ounce\n\n"
+
+            "===Old russian===\n"
+            "3. Pud\n"
+            << std::endl;
+        std::cin >> in_type;
+        std::cout << "How much?" << std::endl;
+        std::cin >> amount;
+        std::cout <<
+            "What mass type do you need?\n"
+            "===Metric system===\n"
+            "1. Ton\n\n"
+
+            "===USA===\n"
+            "2. Ounce\n\n"
+
+            "===Old russian===\n"
+            "3. Pud\n"
+            << std::endl;
+        std::cin >> out_type;
+        try {
+            std::cout << "Result " << mc.fromKilograms((MassType)out_type, mc.toKilograms((MassType)in_type, amount)) << std::endl;
+        }
+        catch (std::invalid_argument e) { std::cout << e.what() << std::endl; }
+        break;
+    case 3: //Area
+        AreaConverter ac;
+        std::cout <<
+            "What area type do you have?\n"
+            "===Metric===\n"
+            "1. Square kilometer\n"
+
+            "===USA===\n"
+            "2. Square mile\n\n"
+
+            "===Old russian===\n"
+            "3. Square arshin\n\n"
+            << std::endl;
+        std::cin >> in_type;
+        std::cout << "How much?" << std::endl;
+        std::cin >> amount;
+        std::cout <<
+            "What area type do you need?\n"
+            "===Metric===\n"
+            "1. Square kilometer\n"
+
+            "===USA===\n"
+            "2. Square mile\n\n"
+
+            "===Old russian===\n"
+            "3. Square arshin\n\n"
+            << std::endl;
+        std::cin >> out_type;
+        try {
+            std::cout << "Result " << ac.fromSquareMeters((AreaType)out_type, ac.toSquareMeters((AreaType)in_type, amount)) << std::endl;
+        }
+        catch (std::invalid_argument e) { std::cout << e.what() << std::endl; }
+        break;
+    default:
+        throw std::invalid_argument("Unsupported unit");
+    }
+
+    return 0;
 }
