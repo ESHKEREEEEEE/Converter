f076ba358a1630f0ba45fcfd8765348a23f225db
ESHKEREEEEEE <146151389+ESHKEREEEEEE@users.noreply.github.com>
2026-04-19 21:37:27 +0700

    10 ounce to 0.283495 kilograms test (already passing)

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -291,5 +291,20 @@ namespace MassConversionTests {
 				);
 			Assert::IsTrue(abs(conversion_result - target) <= EPS, msg.c_str());
 		}
+
+		TEST_METHOD(MassConverter_Convert10OunceToKilograms_Returns0p283495) {
+			MassConverter mc;
+			double input = 10;
+			double target = 0.283495;
+			MassType type = MassType::ounce;
+			double conversion_result = mc.toKilograms(type, input);
+			std::wstring msg = (
+				L"Conversion result of " + std::to_wstring(input)
+				+ L" of type " + std::to_wstring(type)
+				+ L" should be equal to " + std::to_wstring(target)
+				+ L"; got " + std::to_wstring(conversion_result)
+				);
+			Assert::IsTrue(abs(conversion_result - target) <= EPS, msg.c_str());
+		}
 	};
 }
