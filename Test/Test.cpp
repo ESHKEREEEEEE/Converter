6540bee3216f435c1f431d9c2a96553088934990
ESHKEREEEEEE <146151389+ESHKEREEEEEE@users.noreply.github.com>
2026-04-19 22:28:56 +0700

    10000000 square meters to 10 square kilometers test

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -445,6 +445,20 @@ namespace AreaConversionTests {
 				);
 			Assert::IsTrue(abs(conversion_result - target) <= EPS, msg.c_str());
 		}
+		TEST_METHOD(AreaConverter_Convert10000000SquareMetersToSquareKilometers_Returns10) {
+			AreaConverter ac;
+			double input = 10000000;
+			double target = 10;
+			AreaType type = AreaType::square_kilometer;
+			double conversion_result = ac.fromSquareMeters(type, input);
+			std::wstring msg = (
+				L"Conversion result of " + std::to_wstring(input) + L" square meters" +
+				+L" to type " + std::to_wstring(type)
+				+ L" should be equal to " + std::to_wstring(target)
+				+ L"; got " + std::to_wstring(conversion_result)
+				);
+			Assert::IsTrue(abs(conversion_result - target) <= EPS, msg.c_str());
+		}
 	};
 	TEST_CLASS(OldRussianConversionTests) {
 		TEST_METHOD(AreaConverter_Convert1SquareArshinToSquareMeters_Returns0p5058) {
