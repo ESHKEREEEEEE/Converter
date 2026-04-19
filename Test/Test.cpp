8523be28f59f92d3ef93788241c0f48820ec7a95
ESHKEREEEEEE <146151389+ESHKEREEEEEE@users.noreply.github.com>
2026-04-19 16:05:42 +0700

    1 arshin to 0.72m test

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -63,4 +63,20 @@ namespace LengthConversionTests
 			Assert::IsTrue(abs(conversion_result - target) <= EPS, msg.c_str());
 		}
 	};
+	TEST_CLASS(OldRussianConversionTests) {
+		TEST_METHOD(LengthConverter_Convert1ArshinToMeters_Returns0p72) {
+			LengthConverter lc;
+			double input = 1;
+			double target = 0.72;
+			LengthType type = LengthType::arshin;
+			double conversion_result = lc.toMeters(type, input);
+			std::wstring msg = (
+				L"Conversion result of " + std::to_wstring(input)
+				+ L" of type " + std::to_wstring(type)
+				+ L" should be equal to " + std::to_wstring(target)
+				+ L"; got " + std::to_wstring(conversion_result)
+				);
+			Assert::IsTrue(abs(conversion_result - target) <= EPS, msg.c_str());
+		}
+	};
 }
