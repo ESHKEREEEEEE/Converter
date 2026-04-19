acb403d15683ccfc4f3325340d734b2c39186856
ESHKEREEEEEE <146151389+ESHKEREEEEEE@users.noreply.github.com>
2026-04-19 22:18:58 +0700

    10 square arshin to 5.058 square meters test

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -445,5 +445,19 @@ namespace AreaConversionTests {
 				);
 			Assert::IsTrue(abs(conversion_result - target) <= EPS, msg.c_str());
 		}
+		TEST_METHOD(AreaConverter_Convert10SquareArshinToSquareMeters_Returns5p058) {
+			AreaConverter ac;
+			double input = 10;
+			double target = 5.058;
+			AreaType type = AreaType::square_arshin;
+			double conversion_result = ac.toSquareMeters(type, input);
+			std::wstring msg = (
+				L"Conversion result of " + std::to_wstring(input)
+				+ L" of type " + std::to_wstring(type)
+				+ L" should be equal to " + std::to_wstring(target)
+				+ L"; got " + std::to_wstring(conversion_result)
+				);
+			Assert::IsTrue(abs(conversion_result - target) <= EPS, msg.c_str());
+		}
 	};
 }
