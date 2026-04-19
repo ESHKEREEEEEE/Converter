286df30fedfe547d90549dcf1ad7163b317b66c7
ESHKEREEEEEE <146151389+ESHKEREEEEEE@users.noreply.github.com>
2026-04-19 16:49:16 +0700

    0.0254 meters to 1 inch test passed

--- Converter/Converter.cpp
+++ Converter/Converter.cpp
@@ -8,7 +8,7 @@ enum LengthType {
 
 class LengthConverter {
 public:
-    double toMeters(LengthType type, int value) {
+    double toMeters(LengthType type, double value) {
         double multiplier = 0;
         if (type == LengthType::arshin) { multiplier = 0.72; }
         else if (type == LengthType::inch) { multiplier = 0.0254; }
@@ -16,9 +16,10 @@ public:
         return multiplier * value;
     }
 
-    double fromMeters(LengthType type, int value) {
+    double fromMeters(LengthType type, double value) {
         double multiplier = 0;
         if (type == LengthType::arshin) { multiplier =  1/0.72; }
+        else if (type == LengthType::inch) { multiplier = 1 / 0.0254; }
         else if (type == LengthType::kilometers) { multiplier = 0.001; }
         return multiplier * value;
     }
