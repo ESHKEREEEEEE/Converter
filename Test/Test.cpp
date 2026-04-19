71100e36c70db9b0295b83330006e28eab83bedc
ESHKEREEEEEE <146151389+ESHKEREEEEEE@users.noreply.github.com>
2026-04-19 22:40:45 +0700

    25900000 square meters to 10 square mile test (already passing)

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -565,5 +565,19 @@ namespace AreaConversionTests {
 				);
 			Assert::IsTrue(abs(conversion_result - target) <= EPS, msg.c_str());
 		}
+		TEST_METHOD(AreaConverter_Convert25900000SquareMetersToSquareMiles_Returns10) {
+			AreaConverter ac;
+			double input = 2590000;
+			double target = 1;
+			AreaType type = AreaType::square_mile;
+			double conversion_result = ac.fromSquareMeters(type, input);
+			std::wstring msg = (
+				L"Conversion result of " + std::to_wstring(input) + L" square meters" +
+				+L" to type " + std::to_wstring(type)
+				+ L" should be equal to " + std::to_wstring(target)
+				+ L"; got " + std::to_wstring(conversion_result)
+				);
+			Assert::IsTrue(abs(conversion_result - target) <= EPS, msg.c_str());
+		}
 	};
 }
