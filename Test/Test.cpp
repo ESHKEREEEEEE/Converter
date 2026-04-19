c5c375dee621bdf2af8a159c660e55b9f2f86a87
ESHKEREEEEEE <146151389+ESHKEREEEEEE@users.noreply.github.com>
2026-04-19 16:29:04 +0700

    1 m to 0.001 kilometer test

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -62,6 +62,21 @@ namespace LengthConversionTests
 				);
 			Assert::IsTrue(abs(conversion_result - target) <= EPS, msg.c_str());
 		}
+
+		TEST_METHOD(LengthConverter_Convert1MeterToKilometers_Returns0p001) {
+			LengthConverter lc;
+			double input = 1;
+			double target = 0.001;
+			LengthType type = LengthType::kilometers;
+			double conversion_result = lc.fromMeters(type, input);
+			std::wstring msg = (
+				L"Conversion result of " + std::to_wstring(input) + L" meters" +
+				+L" to type " + std::to_wstring(type)
+				+ L" should be equal to " + std::to_wstring(target)
+				+ L"; got " + std::to_wstring(conversion_result)
+				);
+			Assert::IsTrue(abs(conversion_result - target) <= EPS, msg.c_str());
+		}
 	};
 	TEST_CLASS(OldRussianConversionTests) {
 		TEST_METHOD(LengthConverter_Convert1ArshinToMeters_Returns0p72) {
