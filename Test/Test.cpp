d7bb4c98efe7d652f862b501159fbb7ececd7f62
ESHKEREEEEEE <146151389+ESHKEREEEEEE@users.noreply.github.com>
2026-04-19 15:56:26 +0700

    Convert 1 km to 1000 m test

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -2,6 +2,8 @@
 #include "CppUnitTest.h"
 #include "../Converter/Converter.cpp"
 
+#define EPS 0.0000001
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace CreationTests
@@ -22,6 +24,28 @@ namespace CreationTests
 		TEST_METHOD(AreaConverterClassCreation) {
 			AreaConverter* ac = new AreaConverter();
 			Assert::IsNotNull(ac);
+		}
+	};
+}
+
+namespace LengthConversionTests 
+{
+	TEST_CLASS(MetricConversionTests)
+	{
+		TEST_METHOD(LengthConverter_Convert1KilometerToMeters_Returns1000) 
+		{
+			LengthConverter lc;
+			double input = 1;
+			double target = 1000;
+			LengthType type = LengthType::kilometers;
+			double conversion_result = lc.toMeters(type, input);
+			std::wstring msg = (
+				L"Conversion result of " + std::to_wstring(input)
+				+ L" of type " + std::to_wstring(type)
+				+ L" should be equal to " + std::to_wstring(target)
+				+ L"; got " + std::to_wstring(conversion_result)
+				);
+			Assert::IsTrue((abs(conversion_result - target) <= EPS), msg.c_str());
 		}
 	};
 }
