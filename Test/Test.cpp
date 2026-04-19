caea213b02f51c51e24d238d09f1bbdc66bca532
ESHKEREEEEEE <146151389+ESHKEREEEEEE@users.noreply.github.com>
2026-04-19 21:26:42 +0700

    1 pud to 16.380 kilograms test

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -246,4 +246,20 @@ namespace MassConversionTests {
 			Assert::IsTrue(abs(conversion_result - target) <= EPS, msg.c_str());
 		}
 	};
+	TEST_CLASS(OldRussianConversionTests) {
+		TEST_METHOD(MassConverter_Convert1PudToKilograms_Returns16p380) {
+			MassConverter mc;
+			double input = 1;
+			double target = 16.380;
+			MassType type = MassType::pud;
+			double conversion_result = mc.toKilograms(type, input);
+			std::wstring msg = (
+				L"Conversion result of " + std::to_wstring(input)
+				+ L" of type " + std::to_wstring(type)
+				+ L" should be equal to " + std::to_wstring(target)
+				+ L"; got " + std::to_wstring(conversion_result)
+				);
+			Assert::IsTrue(abs(conversion_result - target) <= EPS, msg.c_str());
+		}
+	};
 }
